26b3ce3cc90d9db4a8e51bc528c693dead89da22
Kurdi Botond <kurdi.boti@gmail.com>
2020-10-05 16:26:13 +0200

    basic player movement implemented

---
+++ ThirdPersonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 15;
+    private Vector3 moveDir;
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = transform.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        moveDir = new Vector3(horizontal, 0, vertical).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
+    }
+}
