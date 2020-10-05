c4276758886aecd5bc7706e72edca2b311b8720b
Kurdi Botond <kurdi.boti@gmail.com>
2020-10-05 17:09:47 +0200

    basic player controller implemented, jump and double jump implemented

--- ThirdPersonController.cs
+++ ThirdPersonController.cs
@@ -6,8 +6,16 @@ public class ThirdPersonController : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed = 15;
+    [SerializeField]
+    private float jumpForce = 220;
+    [SerializeField]
+    private LayerMask groundedMask;
+
     private Vector3 moveDir;
-    Rigidbody rigidbody;
+    private Rigidbody rigidbody;
+    private bool grounded;
+    private bool isJumpEnabled;
+    private int numberOfJumps = 0;
 
     private void Start()
     {
@@ -19,6 +27,27 @@ public class ThirdPersonController : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         moveDir = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (Input.GetButtonDown("Jump") && (grounded || isJumpEnabled))
+        {
+            rigidbody.AddForce(transform.up * jumpForce);
+            numberOfJumps++;
+        }
+
+        if(numberOfJumps > 1) {
+            isJumpEnabled = false;
+            numberOfJumps = 0;
+        }
+
+        grounded = false;
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit raycastHit;
+
+        if(Physics.Raycast(ray, out raycastHit, 2 + .1f , groundedMask))
+        {
+            grounded = true;
+            isJumpEnabled = true;
+        }
     }
 
     private void FixedUpdate()
