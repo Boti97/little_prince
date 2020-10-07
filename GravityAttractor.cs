3eb2c794c4c7e8dfcf0d38235719d9fb016cf489
Kurdi Botond <kurdi.boti@gmail.com>
2020-10-07 23:12:41 +0200

    smoother jump implemented from one planet to another

--- GravityAttractor.cs
+++ GravityAttractor.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class GravityAttractor : MonoBehaviour
 {
-    public float gravity = -10f;
-    
+    [SerializeField]
+    private float gravity = -10f;
+    [SerializeField]
+    private float turnSpeed = 0.1f;
+
+
     public void Attract(GameObject body)
     {
         Vector3 targetDir = (body.transform.position - transform.position).normalized;
         Vector3 bodyUp = body.transform.up;
+        Quaternion targetRotation = Quaternion.FromToRotation(bodyUp, targetDir) * body.transform.rotation;
 
-        body.transform.rotation = Quaternion.FromToRotation(bodyUp, targetDir) * body.transform.rotation;
+
+        body.transform.rotation = Quaternion.Slerp(body.transform.rotation, targetRotation, turnSpeed);
         body.GetComponent<Rigidbody>().AddForce(targetDir * gravity);
     }
 }
